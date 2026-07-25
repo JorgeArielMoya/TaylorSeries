c844c7dbe4c90f2e5fa1aeee43feab61dd18cca3
jorge <jorge@LAPTOP-D8GDLQRL>
2026-07-25 15:36:17 -0400

    Crea el modelo de la serie de taylor con sus respectivos atributos

---
+++ ProyectoTaylor/Models/Taylor.cs
@@ -0,0 +1,8 @@
+﻿namespace ProyectoTaylor.Models;
+
+public class Taylor
+{
+    public string Funcion { get; set; } = string.Empty;
+    public string ValorC { get; set; } = string.Empty;
+    public int CantPolinomios { get; set; } = 1;
+}
