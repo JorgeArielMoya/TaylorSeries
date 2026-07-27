1205f117f8fb84b01d31299407220ce600423974
jorge <jorge@LAPTOP-D8GDLQRL>
2026-07-27 16:28:32 -0400

    Fix: Arregla la regla validacion para que valide el rango

--- ProyectoTaylor/Models/TaylorModel.cs
+++ ProyectoTaylor/Models/TaylorModel.cs
@@ -7,6 +7,6 @@ public class TaylorModel
     public string Funcion { get; set; } = string.Empty;
     public string ValorC { get; set; } = string.Empty;
 
-    [Required(ErrorMessage = "La cantidad de polinomios debe ser mayor que 0")]
+    [Range(1, int.MaxValue, ErrorMessage = "La cantidad de polinomios debe ser mayor que 0")]
     public int CantPolinomios { get; set; } = 1;
 }
