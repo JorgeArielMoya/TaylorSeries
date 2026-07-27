0aae056837d389c4992a48fe4a3e235a3d913a6f
jorge <jorge@LAPTOP-D8GDLQRL>
2026-07-27 16:20:52 -0400

    Feat: Agrega el mensaje de validacion a la cantidad de polinomios

--- ProyectoTaylor/Models/TaylorModel.cs
+++ ProyectoTaylor/Models/TaylorModel.cs
@@ -1,8 +1,12 @@
-﻿namespace ProyectoTaylor.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoTaylor.Models;
 
 public class TaylorModel
 {
     public string Funcion { get; set; } = string.Empty;
     public string ValorC { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "La cantidad de polinomios debe ser mayor que 0")]
     public int CantPolinomios { get; set; } = 1;
 }
